a14a4e6bb2a2f122acdd4f739efab370d61b0543
rockerest <admin@rockerest.com>
2014-02-24 22:25:30 -0600

    Moving the lib dir up a level to vendor & Adding a bunch of unstyled content

--- js/app/resume/bootstrap.js
+++ js/app/resume/bootstrap.js
@@ -1,10 +1,10 @@
 requirejs.config({
     "urlArgs": "_=" + (new Date()).getTime(),
     "paths": {
-        "lib":  "../../lib",
+        "lib":  "../../../vendor",
 
-        "underscore": "../../underscore/underscore",
-        "jquery": ["//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min", "../../jquery/dist/jquery.min" ]
+        "underscore": "../../../vendor/underscore/underscore",
+        "jquery": ["//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min", "../../../vendor/jquery/dist/jquery.min" ]
     }
 });
 
