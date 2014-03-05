84b262dda1710248ff2cb135da6c4d549c60429b
rockerest <admin@rockerest.com>
2014-03-04 20:46:04 -0600

    Added click interaction to moreblocks, added expanders

--- js/app/resume/ui.js
+++ js/app/resume/ui.js
@@ -1,7 +1,8 @@
 define(
-    ["jquery"],
-    function( $ ){
-        var Ui = function(){};
+    ["jquery", "underscore"],
+    function( $, _ ){
+        var Ui = function(){},
+            toggleElement, unpackExpandables, repackExpandables;
 
         Ui.prototype.init = function(){
             this.startup();
@@ -11,15 +12,112 @@ define(
 
         Ui.prototype.startup = function(){
             $( ".more" ).hide();
+            $( ".expandables" ).hide();
         };
 
         Ui.prototype.bind = function(){
-            $( "section" ).on({
+            $( ".more" ).parent().on({
+                "click": function(){
+                    toggleElement( $(".more", this) );
+                },
+                "touchstart": function(){
+                    toggleElement( $(".more", this) );
+                },
                 "mouseenter": function(){
-                    $( ".more", this ).stop( true ).slideDown();
+                    var el = $( ".more", this );
+                    if( !el.data( "frozen" ) && !el.data( "locked" ) ){
+                        $( ".more", this ).stop( true ).slideDown();
+                    }
                 },
                 "mouseleave": function(){
-                    $( ".more", this ).stop( true ).slideUp();
+                    var el = $( ".more", this );
+                    if( !el.data( "frozen" ) && !el.data( "locked" ) ){
+                        $( ".more", this ).stop( true ).slideUp();
+                    }
+                }
+            });
+
+            $( ".expand" ).on({
+                "click": function( e ){
+                    var button      = $( this ),
+                        moreBlock   = button.closest( ".more" ),
+                        current     = moreBlock.closest( ".column" ),
+                        html        = button.html();
+
+                    if( html.indexOf( "More" ) > -1 ){
+                        html = html.replace( "More", "Less" );
+                        unpackExpandables( $( ".expandables", current ).children(), current );
+
+                        moreBlock.data( "locked", true );
+                    }
+                    else if( html.indexOf( "Less" ) > -1 ){
+                        html = html.replace( "Less", "More" );
+                        repackExpandables( current.siblings() );
+
+                        moreBlock.data( "locked", false );
+                        moreBlock.data( "frozen", false );
+                    }
+
+                    button
+                        .html( html )
+                        .find( ".fa-expand, .fa-compress" )
+                        .toggleClass( "fa-expand fa-compress" );
+
+                    e.stopPropagation();
+                }
+            });
+        };
+
+        toggleElement = function( element ){
+            var frozen = typeof element.data( "frozen" ) !== "undefined" ? element.data( "frozen" ) : false,
+                locked = typeof element.data( "locked" ) !== "undefined" ? element.data( "locked" ) : false;
+
+            if( !locked ){
+                if( frozen ){
+                    element.show();
+                }
+
+                element.data( "frozen", !frozen );
+            }
+        };
+
+        unpackExpandables = function( expanders, column ){
+            column.siblings().hide();
+
+            var befores = _( expanders ).filter( function( el ){
+                    return $( el ).data( "placement" ) === "before";
+                }),
+                afters = _( expanders ).filter( function( el ){
+                    return $( el ).data( "placement" ) === "after";
+                }),
+                unpack = function( list, sibling ){
+                    var i, clone;
+
+                    for( i = list.length; i > 0; i-- ){
+                        clone = $( list[i - 1] )
+                            .clone()
+                            .attr( "class", sibling.attr( "class" ) );
+
+                        if( clone.data( "placement" ) === "before" ){
+                            clone.insertBefore( sibling );
+                        }
+                        else if( clone.data( "placement" ) === "after" ){
+                            clone.insertAfter( sibling );
+                        }
+                    }
+                };
+
+            unpack( befores, column );
+            unpack( afters, column );
+        };
+
+        repackExpandables = function( sibs ){
+            sibs.each( function( i, el ){
+                if( $(el).attr( "data-order" ) ){
+                    $(el).remove();
+                }
+                else{
+                    $(el).show();
                 }
             });
         };
