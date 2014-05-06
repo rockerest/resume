8f71a755e12595707e102589c8d2c63490f2a8e6
rockerest <admin@rockerest.com>
2014-05-05 23:50:44 -0500

    Adding Events

---
+++ js/app/events/main.js
@@ -0,0 +1,16 @@
+define(
+    ["jquery"],
+    function( $ ){
+        var MainEvents = function(){};
+
+        MainEvents.prototype.registerEvents = function(){
+            this.registerMainEvents();
+        };
+
+        MainEvents.prototype.registerMainEvents = function(){
+
+        };
+
+        return MainEvents;
+    }
+);
