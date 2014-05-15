ac99e25a24d3c82c0d8a5e0023132db3e9e898d5
rockerest <websites@thomasrandolph.info>
2014-05-15 01:22:03 -0500

    Bind events and handle bindings in the correct places so they always work

--- js/app/bindings/header.js
+++ js/app/bindings/header.js
@@ -1,12 +1,25 @@
 define(
-    ["jquery"],
-    function( $ ){
-        var HeaderBinding = {};
+    ["jquery", "interface/header"],
+    function( $, ui ){
+        var HeaderBinding = {},
+            HeaderUi = new ui();
 
         HeaderBinding.header = function(){
             $( '#logo-container' ).on( "click", function( e ){
                 $( this ).trigger( "resume.click.header.logo" );
                 return false;
+            });
+
+            $( '#startIntro' ).on({
+                "click": function(){
+                    $( this ).trigger( "resume.click.main/help/start" );
+                },
+                "mouseenter": function(){
+                    HeaderUi.spin( $( "i", this)[0] );
+                },
+                "mouseleave": function(){
+                    HeaderUi.unSpin( $( "i", this)[0] );
+                }
             });
         };
 
--- js/app/events/main.js
+++ js/app/events/main.js
@@ -1,14 +1,17 @@
 define(
-    ["jquery"],
-    function( $ ){
-        var MainEvents = function(){};
+    ["jquery", "interface/main"],
+    function( $, ui ){
+        var MainEvents = function(){},
+            MainUi = new ui();
 
         MainEvents.prototype.registerEvents = function(){
             this.registerMainEvents();
         };
 
         MainEvents.prototype.registerMainEvents = function(){
-
+            $( document ).on( "resume.click.main/help/start", function( e ){
+                MainUi.startIntroduction();
+            });
         };
 
         return MainEvents;
